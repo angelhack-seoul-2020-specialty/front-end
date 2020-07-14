36fcc672060e352969b245d6c409f2c878b82cca
Pinot. Kim <pinot.kim@kakao.com>
2020-07-15 04:59:59 +0900

    :sparkles: iconButton atom 추가

---
+++ src/component/UI/atoms/iconButton.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const IconButton = styled.button`
+  display: inline-flex;
+  background-color: transparent;
+  border: none;
+  font-size: 1.65em;
+  cursor: pointer;
+`;
+
+export { IconButton }
