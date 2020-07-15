c6e0a0cb7f13eaae48ee18d14e30b74054aecb84
Pinot. Kim <pinot.kim@kakao.com>
2020-07-15 20:58:25 +0900

    :lipstick: 배경이 있는 링크 atom 추가

---
+++ src/component/UI/atoms/linkWithBG.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const LinkWithBG = styled.span`
+  background: no-repeat url("/static/asset/link_background.svg") center;
+  font-weight: bold;
+  
+  a {
+    padding: 1.5px 20px;
+    color: #fff;
+    text-decoration: none;
+  }
+`;
+
+export {LinkWithBG}
