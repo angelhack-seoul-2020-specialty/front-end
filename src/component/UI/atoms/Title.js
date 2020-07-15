11bea95c4b924091ec63e53f3280d3676aed9ca9
Pinot. Kim <pinot.kim@kakao.com>
2020-07-15 20:58:46 +0900

    :lipstick: Title 컴포넌트 font-size 변경

--- src/component/UI/atoms/Title.js
+++ src/component/UI/atoms/Title.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Title = styled.h2`
-  font-size: ${(props) => props.card ? '1.2em' : '2em'};
+  font-size: ${(props) => props.card ? '1.2em' : '1.9em'};
   letter-spacing: 0.72px;
 `;
 
