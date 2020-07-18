dbb8f46ba2e87635f826193ff2235e8c786c51cf
Pinot. Kim <pinot.kim@kakao.com>
2020-07-19 07:19:13 +0900

    :sparkles: 토큰 유효성을 검증하는 기능 추가
    페이지가 이동될 때마다 토큰이 있는지 검사하고, 해당 토큰이 유효한지 답변의 크기가 가장 작은 기능으로 리퀘스트를 날리는 기능을 만들었습니다.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import { Switch, withRouter } from 'react-router-dom';
 import { FancyRoute } from './component/tools/fancyRoute';
 import { Helmet } from 'react-helmet';
 import routes from './routes/routes';
+import {query} from './lib/query';
+import {decodeToken, getToken} from './lib/token';
 
-function App({history}) {
+function App({history, location}) {
   useEffect(() => {
     if (!document.cookie.includes("access_token_cookie")) {
       console.log('redirect')
       history.replace('/login')
     }
-  }, [history])
+    
+    const user = decodeToken(getToken());
+  
+    query({
+      method: 'get',
+      url: `/api/${user.identity}/amount`,
+    })
+        .catch(err => {
+          const error = err.toString()
+          
+          if (error.includes('401')) {
+            alert('인증 토큰이 만료되었습니다. 다시 로그인해주세요.');
+            document.cookie = "access_token_cookie=; expires=Thu, 01 Jan 1999 00:00:10 GMT;";
+            history.replace('/login')
+          }
+        });
+  }, [history, location.pathname])
   
   return (
     <>
