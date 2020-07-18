7ead47ecc2e09ad87b2088131ea2b4e5e234871e
Pinot. Kim <pinot.kim@kakao.com>
2020-07-19 00:11:52 +0900

    :sparkles: 토큰과 관련된 기능들 추가
    - 토큰을 가져가고, 토큰을 해석하는 기능을 추가했습니다.

---
+++ src/lib/token.js
@@ -0,0 +1,9 @@
+const getToken = () => {
+  return document.cookie.match(new RegExp("(^| )access_token_cookie=([^;]+)"))[2] || null;
+};
+
+const decodeToken = (string) => {
+  return JSON.parse(window.atob(string.split('.')[1]))
+}
+
+export {getToken, decodeToken}
